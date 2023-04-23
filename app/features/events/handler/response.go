538f41e3a8c66e1d9a4939e5b729a51a754b2057
dimasyudhana <dimas.yudhana@gmail.com>
2023-04-23 10:25:23 +0700

    feature/events/on-progress

--- app/features/events/handler/response.go
+++ app/features/events/handler/response.go
@@ -1,1 +1,12 @@
 package handler
+
+type ResponseGetEvents struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	EventDate   string `json:"eventdate"`
+	EventTime   string `json:"eventtime"`
+	Status      string `json:"status"`
+	Category    string `json:"category"`
+	Location    string `json:"location"`
+	Image       string `json:"image"`
+}
